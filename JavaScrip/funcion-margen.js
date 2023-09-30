03c44b0c34049d65ef86ac9eeeb5b5ee70dea87f
Martin Caresia <107585462+zyro2512@users.noreply.github.com>
2023-09-30 01:59:05 -0300

    1.0

---
+++ JavaScrip/funcion-margen.js
@@ -0,0 +1,15 @@
+function cambiarColorAutomatico() {
+  const div = document.getElementById('bordes-cambiantes');
+  
+  setInterval(function() {
+    if (div.classList.contains('borde-verde')) {
+      div.classList.remove('borde-verde');
+      div.classList.add('borde-azul');
+    } else {
+      div.classList.remove('borde-azul');
+      div.classList.add('borde-verde');
+    }
+  }, 1000); // Cambio cada 2 segundos (2000 milisegundos)
+}
+
+cambiarColorAutomatico(); // Inicia el cambio automático de color al cargar la página
